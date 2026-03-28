e0746a64ab2c53f4384314ba7600803d1252a124
ericasaline <ericasaline@gmail.com>
2026-03-28 10:08:47 -0300

    feat: adds transition to splash screen

--- app/src/main/java/br/com/app/markae/MainActivity.kt
+++ app/src/main/java/br/com/app/markae/MainActivity.kt
@@ -1,9 +1,11 @@
 package br.com.app.markae
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import br.com.app.markae.ui.navigation.MarkaeNavGraph
 import br.com.app.markae.ui.theme.MarkaeTheme
 
@@ -11,6 +13,15 @@ class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		enableEdgeToEdge()
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+			installSplashScreen().setOnExitAnimationListener { splash ->
+				splash.view.animate()
+					.alpha(0f)
+					.setDuration(1000)
+					.withEndAction { splash.remove() }
+					.start()
+			}
+		}
 		setContent {
 			MarkaeTheme {
 				MarkaeNavGraph()
